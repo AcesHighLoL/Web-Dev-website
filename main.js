487872d85063d19c788acaa035e7386721cef8c6
George <georgeguardia01@gmail.com>
2022-03-22 17:19:47 -0400

    Slide show

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+function auto(){
+    var counter = 1;
+    setInterval(function(){
+        document.getElementById('slidebtn'+ counter).checked = true;
+        counter++;
+        if(counter > 4){
+            counter =1;
+        }
+    },1000);
+
+}
